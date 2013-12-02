7b987300754da9cda22d26a582f3db165c54dc3e
Henry Lee <manageyp@gmail.com>
2013-12-02 13:39:11 +0800

    [Change] database settings use mysql

--- django_tutorials/settings.py
+++ django_tutorials/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'django_tutorials.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'college_dev',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': '',
+        'PORT': '3306'
     }
 }
 
